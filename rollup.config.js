f0e72ef7be7c2e7dad59a07517a17f7e477f64bd
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-11-13 16:07:13 +0800

    chore: support typescript

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel'
-import typeScript from '@rollup/plugin-typescript'
+import typeScript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss';
 import autoprefixer from 'autoprefixer';
 import terser from '@rollup/plugin-terser';
@@ -11,32 +11,36 @@ const config = defineConfig({
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs',
+      file: 'dist/bundle.es.js',
+      format: 'es',
     },
-    {
-      file: 'dist/bundle.iife.js',
-      format: 'iife',
-      name: 'Tun',
-    }
+    // {
+    //   file: 'dist/bundle.cjs.js',
+    //   format: 'cjs',
+    // },
+    // {
+    //   file: 'dist/bundle.iife.js',
+    //   format: 'iife',
+    //   name: 'Tun',
+    // }
   ],
   external: ['react'],
   plugins: [
-    typeScript(),
     resolve(),
     commonjs(),
     babel({
       babelHelpers: 'bundled',
-      exclude:'node_modules/**'
+      exclude:'node_modules/**',
     }),
     terser(),
+    typeScript(),
     postcss({
       plugins: [  
         autoprefixer(),
       ],
-      // extract: 'css/index.css',
+      extract: 'css/index.css',
     }),
-  ]
+  ],
 });
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import './index.scss';
 
+// export { Test } from './test';
+
 console.log('ts');
+
+export const test = async () => {
+	const testData = [5, 2, 4, 1];
+	testData.sort((a, b) => a - b);
+	console.log(testData);
+};
